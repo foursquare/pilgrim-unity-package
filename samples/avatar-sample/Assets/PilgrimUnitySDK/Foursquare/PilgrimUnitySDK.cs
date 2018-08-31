909639bf61095c61975b0a683c574a460c8c63a5
Brian Rojas <marchinram@gmail.com>
2018-08-31 17:08:02 -0500

    Sample app work and updated avatar sample

--- samples/avatar-sample/Assets/PilgrimUnitySDK/Foursquare/PilgrimUnitySDK.cs
+++ samples/avatar-sample/Assets/PilgrimUnitySDK/Foursquare/PilgrimUnitySDK.cs
@@ -42,7 +42,7 @@ namespace Foursquare
 
 		public static void Start(string consumerKey, string consumerSecret) {}
 
-		private static string DoGetLogs() { return "{\"Items\":[]}"; }
+		private static string DoGetLogs() { return "{\"Items\":[{\"title\":\"api-request\",\"description\":\"POST: https://sdk.foursquare.com/v2/TMKHTRWRRYO4WIZPVJNHA1Q3JU0YBED5XIONMQTOC00YYCLY/pilgrim/enable\",\"timestamp\":1535160576},{\"title\":\"saved home/work regions\",\"description\":\"()\",\"timestamp\":1535160576},{\"title\":\"recalculating home/work\",\"description\":\"{    previousCalculationDate = \\\"<null>\\\";}\",\"timestamp\":1535160576},{\"title\":\"No home/work file or error reading it\",\"description\":\"Error Domain=NSCocoaErrorDomain Code=260 \\\"The file “FSQPRegions.archive” couldn’t be opened because there is no such file.\\\" UserInfo={NSFilePath=/var/mobile/Containers/Data/Application/01CEF01E-9C26-4063-A0C5-FE9FB7EF93B6/Documents/FSQPRegions.archive, NSUnderlyingError=0x1c464d650 {Error Domain=NSPOSIXErrorDomain Code=2 \\\"No such file or directory\\\"}}\",\"timestamp\":1535160576},{\"title\":\"api-request\",\"description\":\"POST: https://sdk.foursquare.com/v2/TMKHTRWRRYO4WIZPVJNHA1Q3JU0YBED5XIONMQTOC00YYCLY/pilgrim/install\",\"timestamp\":1535160576}]}"; }
 
 		#endif
 
---
+++ source/Assets/Scripts/MainSceneManager.cs
@@ -0,0 +1,89 @@
+﻿using Foursquare;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneManager : MonoBehaviour 
+{
+
+	private enum Mode {
+		Visits,
+		GeofenceEvents,
+		Logs
+	}
+
+
+	private Mode mode;
+
+	private Vector2 scrollPosition = Vector2.zero;
+
+	private Texture2D bgTex;
+
+	void OnGUI()
+	{	
+		if (!bgTex) {
+			bgTex = new Texture2D(1, 1);
+			Color fillColor = new Color32(0, 0, 0, 80);
+ 			Color[] fillColorArray =  bgTex.GetPixels();
+ 
+ 			for (var i = 0; i < fillColorArray.Length; i++) {
+     			fillColorArray[i] = fillColor;
+ 			}
+  
+ 			bgTex.SetPixels(fillColorArray);
+ 			bgTex.Apply();
+		}
+
+		GUILayout.BeginVertical();
+
+		GUILayout.Space(20);
+
+		GUILayout.BeginHorizontal();
+
+		if (GUILayout.Button("Visits", GUILayout.Height(50.0f))) {
+			mode = Mode.Visits;
+		}
+		if (GUILayout.Button("Geofence Events", GUILayout.Height(50.0f))) {
+			mode = Mode.GeofenceEvents;
+		}
+		if (GUILayout.Button("Logs", GUILayout.Height(50.0f))) {
+			mode = Mode.Logs;
+		}
+
+		GUILayout.EndHorizontal();
+
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width));
+
+		GUILayout.BeginVertical();
+
+		switch (mode) {
+			case Mode.Logs:
+				OnLogsGUI();
+				break;
+			default:
+				break;
+		}
+
+        GUILayout.EndVertical();
+
+		GUILayout.EndScrollView();
+
+		GUILayout.EndVertical();
+	}
+
+	private void OnLogsGUI()
+	{
+		List<Log> logs = PilgrimUnitySDK.GetLogs();
+		for (int i = 0; i < logs.Count; i++) {
+			Log log = logs[i];
+			GUIStyle gs = new GUIStyle();
+			if (i % 2 == 0) {
+				gs.normal.background = bgTex;
+			}
+			GUILayout.BeginVertical(gs);
+			GUILayout.Label("<size=20>" + log.Title + " @ " + log.Timestamp + "</size>");
+			GUILayout.Label("<size=15>" + log.Description + "</size>");
+			GUILayout.EndVertical();
+		}
+	}
+	
+}
