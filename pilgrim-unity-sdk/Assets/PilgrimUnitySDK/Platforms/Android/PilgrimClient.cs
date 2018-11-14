ade434da499bc9c53187a603ffe9bafe9d95589f
Brian Rojas <marchinram@gmail.com>
2018-11-14 15:31:47 -0600

    Android client cleanup

--- pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Platforms/Android/PilgrimClient.cs
+++ pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Platforms/Android/PilgrimClient.cs
@@ -1,11 +1,12 @@
 #if UNITY_ANDROID
 
+using System;
 using UnityEngine;
 
 namespace Foursquare.Android
 {
 
-    public class PilgrimClient : AndroidJavaProxy, IPilgrimClient
+    public class PilgrimClient : AndroidJavaProxy, IPilgrimClient, IDisposable
     {
 
         public event LocationPermissionsGranted OnLocationPermissionsGranted;
@@ -14,31 +15,34 @@ namespace Foursquare.Android
 
         public PilgrimClient() : base("com.foursquare.pilgrimunitysdk.PilgrimClientListener")
         {
-            var playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            var activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity");
-            pilgrimClient = new AndroidJavaObject("com.foursquare.pilgrimunitysdk.PilgrimClient", activity, this);
+            using (var playerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            using (var activity = playerClass.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                pilgrimClient = new AndroidJavaObject("com.foursquare.pilgrimunitysdk.PilgrimClient", activity, this);
+            }
         }
 
         public void SetUserInfo(PilgrimUserInfo userInfo)
         {
-            var userInfoMap = new AndroidJavaObject("java.util.HashMap");
-            var putMethod = AndroidJNIHelper.GetMethodID(userInfoMap.GetRawClass(), "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
-            
-            object[] args = new object[2];
-            foreach (var pair in userInfo.BackingStore) {
-                if (pair.Value == null) {
-                    continue;
-                }
-                using (AndroidJavaObject k = new AndroidJavaObject("java.lang.String", pair.Key)) {
-                    using (AndroidJavaObject v = new AndroidJavaObject("java.lang.String", pair.Value)) {
+            using (var userInfoMap = new AndroidJavaObject("java.util.HashMap"))
+            {
+                var putMethod = AndroidJNIHelper.GetMethodID(userInfoMap.GetRawClass(), "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+                object[] args = new object[2];
+                foreach (var pair in userInfo.BackingStore) {
+                    if (pair.Value == null) {
+                        continue;
+                    }
+                    using (AndroidJavaObject k = new AndroidJavaObject("java.lang.String", pair.Key))
+                    using (AndroidJavaObject v = new AndroidJavaObject("java.lang.String", pair.Value)) 
+                    {
                         args[0] = k;
                         args[1] = v;
                         AndroidJNI.CallObjectMethod(userInfoMap.GetRawObject(), putMethod, AndroidJNIHelper.CreateJNIArgArray(args));
                     }
                 }
-            }
 
-            pilgrimClient.Call("setUserInfo", userInfoMap);
+                pilgrimClient.Call("setUserInfo", userInfoMap);
+            }
         }
 
         public void RequestLocationPermissions()
@@ -59,6 +63,11 @@ namespace Foursquare.Android
         public void ClearAllData()
         {
             pilgrimClient.Call("clearAllData");
+        }
+
+        public void Dispose()
+        {
+            pilgrimClient.Dispose();
         }
 
         public void onLocationPermissionResult(bool granted) {
