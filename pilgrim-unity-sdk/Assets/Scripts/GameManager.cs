8f04aa0199f1261d08772971f68e985903cfa3fd
Brian Rojas <marchinram@gmail.com>
2018-11-15 16:01:05 -0600

    Worked on sample scene

--- pilgrim-unity-sdk/Assets/Scripts/GameManager.cs
+++ pilgrim-unity-sdk/Assets/Scripts/GameManager.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour 
 {
 
+	private enum NextAction
+	{
+		START,
+		GET_CURRENT_LOCATION
+	}
+
+	private NextAction nextAction;
+
 	void OnEnable()
 	{
 		PilgrimUnitySDK.OnLocationPermissionsResult += OnLocationPermissionsResult;
@@ -17,47 +25,41 @@ public class GameManager : MonoBehaviour
 		PilgrimUnitySDK.OnGetCurrentLocationResult -= OnGetCurrentLocationResult;
 	}
 
-	void Start() 
+	public void OnPressStart()
 	{
-		// PilgrimUserInfo userInfo = new PilgrimUserInfo();
-		// userInfo.Set("a", "b");
-		// userInfo.Set("c", "d");
-		// userInfo.SetBirthday(new System.DateTime(1984, 11, 26));
-		// userInfo.SetGender(PilgrimUserInfo.Gender.Male);
-		// userInfo.SetUserId("marchinram");
-		// PilgrimUnitySDK.SetUserInfo(userInfo);
-
-		// yield return new WaitForSeconds(10.0f);
-
-		// userInfo = new PilgrimUserInfo();
-		// userInfo.Set("a", null);
-		// userInfo.Set("c", "d");
-		// userInfo.SetBirthday(new System.DateTime(1984, 11, 26));
-		// userInfo.SetGender(PilgrimUserInfo.Gender.Male);
-		// userInfo.SetUserId(null);
-		// PilgrimUnitySDK.SetUserInfo(userInfo);
-
-		
+		nextAction = NextAction.START;
 		PilgrimUnitySDK.RequestLocationPermissions();
 	}
 
+	public void OnPressStop()
+	{
+		PilgrimUnitySDK.Stop();
+	}
+
+	public void OnPressClearData()
+	{
+		PilgrimUnitySDK.ClearAllData();
+	}
+
+	public void OnPressGetCurrentLocation()
+	{
+		nextAction = NextAction.GET_CURRENT_LOCATION;
+		PilgrimUnitySDK.RequestLocationPermissions();
+	}
 	private void OnLocationPermissionsResult(bool granted)
 	{
 		if (granted) {
-			// PilgrimUnitySDK.Start();
-			PilgrimUnitySDK.GetCurrentLocation();
+			if (nextAction == NextAction.START) {
+				PilgrimUnitySDK.Start();
+			} else if (nextAction == NextAction.GET_CURRENT_LOCATION) {
+				PilgrimUnitySDK.GetCurrentLocation();
+			}
 		}
 	}
 
 	private void OnGetCurrentLocationResult(bool success, CurrentLocation currentLocation)
 	{
-		if (success) {
-			Debug.Log("CURRENT LOCATION");
-			Debug.Log(currentLocation.CurrentPlace.ArrivalTime);
-			Debug.Log(currentLocation.CurrentPlace.Venue.Name);
-		} else {
-			Debug.Log("CURRENT LOCATION FAILED");
-		}
+		
 	}
 	
 }
