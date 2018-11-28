716ab926cbb173ed002a5008f4ff3d0f32b13813
Brian Rojas <marchinram@gmail.com>
2018-11-28 17:32:16 -0600

    cleaned up build script

--- build.sh
+++ build.sh
@@ -9,9 +9,7 @@ UNITY_COMMAND="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 PILGRIM_UNITY_SDK_VERSION="0.1.0" # TODO Get this value dynamically, maybe from git tag or something?!?
 PILGRIM_UNITY_SDK_PACKAGE="$PROJECT_ROOT/pilgrim-unity-sdk-$PILGRIM_UNITY_SDK_VERSION.unitypackage"
 
-# PLAY_SERVICES_RESOLVER_PACKAGE="$PROJECT_ROOT/"$( find play-services-resolver-*.unitypackage )""
-PLAY_SERVICES_RESOLVER_VERSION="1.2.88.0"
-PLAY_SERVICES_RESOLVER_PACKAGE="$PROJECT_ROOT/play-services-resolver-$PLAY_SERVICES_RESOLVER_VERSION.unitypackage"
+PLAY_SERVICES_RESOLVER_PACKAGE="$PROJECT_ROOT/"$( find play-services-resolver-*.unitypackage )""
 
 # -gvh_disable is needed for Play Services Resolver (https://github.com/googlesamples/unity-jar-resolver#getting-started)
 
