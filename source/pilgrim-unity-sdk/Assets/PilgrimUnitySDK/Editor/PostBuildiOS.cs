35e98fda2cadc1e28821a5cbdf9f6877cc7a8c82
Brian Rojas <marchinram@gmail.com>
2019-01-14 16:00:41 -0600

    Added iOS post build script for location permissions and modules

---
+++ source/pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Editor/PostBuildiOS.cs
@@ -0,0 +1,52 @@
+﻿#if UNITY_IOS
+
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Foursquare
+{
+
+    public static class PostBuildiOS
+    {
+
+        // TODO MAKE THIS CONFIGURABLE IN PILGRIM CONFIG
+        [PostProcessBuild]
+        public static void AddAlwaysAndWhenInUseUsageDescription(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            if (buildTarget == BuildTarget.iOS) {
+                var plistPath = string.Format("{0}/Info.plist", pathToBuiltProject);
+                
+                var plist = new PlistDocument();
+                plist.ReadFromFile(plistPath);
+
+                var root = plist.root;
+
+                var whenInUseUsageDescription = root["NSLocationWhenInUseUsageDescription"];
+                if (whenInUseUsageDescription != null) {
+                    root["NSLocationAlwaysAndWhenInUseUsageDescription"] = whenInUseUsageDescription;
+                }
+
+                plist.WriteToFile(plistPath);
+            }
+        }
+
+        // TODO REMOVE THIS WHEN MODULES ARE REMOVED FROM PILGRIM IN 2.1.1
+        [PostProcessBuild]
+        public static void EnableModules(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            var pbxProj = new PBXProject();
+            pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+
+            var targetName = PBXProject.GetUnityTargetName();
+            var targetGuid = pbxProj.TargetGuidByName(targetName);
+            pbxProj.SetBuildProperty(targetGuid, "CLANG_ENABLE_MODULES", "YES");
+
+            pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+        }
+
+    }
+
+}
+
+#endif
