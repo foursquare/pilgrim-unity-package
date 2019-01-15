8abb20d2ab61477a81bdde5c7ec4db591938df62
Brian Rojas <marchinram@gmail.com>
2019-01-14 22:08:41 -0600

    Mapbox demo work

---
+++ samples/PilgrimMapbox/Assets/PilgrimMapbox/Scripts/Bounce.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+
+    private Vector3 _initialPosition;
+
+    private bool _movingUp = true;
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (_movingUp) {
+            var endPosition = _initialPosition + transform.up * 10.0f;
+            transform.position = Vector3.Lerp(transform.position, endPosition, 3.0f * Time.deltaTime);
+        } else {
+            var endPosition = _initialPosition - transform.up * 10.0f;
+            transform.position = Vector3.Lerp(transform.position, endPosition, 3.0f * Time.deltaTime);
+        }
+
+        if ((transform.position - _initialPosition).magnitude > 9.0f) {
+            _movingUp = !_movingUp;
+        }
+    }
+
+}
