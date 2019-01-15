89febf568fc4bd10e3c66f592bfed865533dd33c
Brian Rojas <marchinram@gmail.com>
2019-01-14 18:13:47 -0600

    Pilgrim mapbox demo work

---
+++ samples/PilgrimMapbox/Assets/PilgrimMapbox/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using Foursquare;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private AbstractMap _map;
+
+    void Start()
+    {
+        PilgrimUnitySDK.OnLocationPermissionResult += OnGetLocationPermission;
+        PilgrimUnitySDK.RequestLocationPermissions();
+    }
+
+    void OnGetLocationPermission(bool granted)
+    {
+        PilgrimUnitySDK.OnLocationPermissionResult -= OnGetLocationPermission;
+        PilgrimUnitySDK.OnGetCurrentLocationResult += OnGetCurrentLocation;
+        PilgrimUnitySDK.GetCurrentLocation();
+    }
+
+    void OnGetCurrentLocation(CurrentLocation currentLocation, Exception exception)
+    {
+        Debug.Log(currentLocation.CurrentPlace == null);
+        Debug.Log(exception);
+        if (currentLocation.CurrentPlace != null) {
+            Debug.Log(currentLocation.CurrentPlace.Venue.Name);
+        }
+
+        PilgrimUnitySDK.OnGetCurrentLocationResult -= OnGetCurrentLocation;
+        
+        var currentPlace = currentLocation.CurrentPlace;
+        if (currentPlace != null) {
+            _map.Initialize(new Vector2d(currentPlace.Location.Latitude, currentPlace.Location.Longitude), 17);
+        }
+    }
+
+}
