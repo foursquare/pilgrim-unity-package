6ae2c8908b0bbb667bf02e13013391e1fb91c4ae
Brian Rojas <marchinram@gmail.com>
2019-01-23 16:12:01 -0600

    Renamed editor client

---
+++ source/pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Platforms/EditorClientClient.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+namespace Foursquare
+{
+
+    public class EditorClient : IPilgrimClient
+    {
+
+        public event Action<bool> OnLocationPermissionResult = delegate { };
+
+        public event Action<CurrentLocation, Exception> OnGetCurrentLocationResult = delegate { };
+
+        public void SetUserInfo(UserInfo userInfo)
+        {
+
+        }
+
+        public void RequestLocationPermissions()
+        {
+            OnLocationPermissionResult(true);
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void ClearAllData()
+        {
+
+        }
+
+        public void GetCurrentLocation()
+        {
+            var editorMock = GameObject.FindObjectOfType<GetCurrentLocationEditorMock>();
+            if (editorMock == null)
+            {
+                editorMock = new GameObject("_PilgrimMockLocation").AddComponent<GetCurrentLocationEditorMock>();
+                editorMock.mockLocation = Resources.Load<CurrentLocationMock>("4sqHQ");
+            }
+            else if (editorMock.mockLocation == null)
+            {
+                OnGetCurrentLocationResult(null, new Exception(string.Format("PilgrimUnitySDK Error: Null mock location on GetCurrentLocationEditorMock script attached to {0}", editorMock.gameObject.name)));
+                return;
+            }
+            var currentLocation = editorMock.mockLocation.CurrentLocation;
+            OnGetCurrentLocationResult(currentLocation, null);
+        }
+
+    }
+
+}
