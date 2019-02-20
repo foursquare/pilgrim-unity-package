fe6169830aa066b9916b568b6b9acdc585954aab
Brian Rojas <marchinram@gmail.com>
2019-02-20 13:40:05 -0600

    Added strip frameworks run phase to generated Xcode project and added checks in all methods to ensure building for iOS

--- source/pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Editor/PostBuildiOS.cs
+++ source/pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Editor/PostBuildiOS.cs
@@ -38,63 +38,89 @@ namespace Foursquare
         [PostProcessBuild]
         public static void AddRunPathSearchPaths(BuildTarget buildTarget, string pathToBuiltProject)
         {
-            var pbxProj = new PBXProject();
-            pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+            if (buildTarget == BuildTarget.iOS)
+            {
+                var pbxProj = new PBXProject();
+                pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
 
-            var targetName = PBXProject.GetUnityTargetName();
-            var targetGuid = pbxProj.TargetGuidByName(targetName);
-            pbxProj.SetBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "$(inherited) '@executable_path/Frameworks' '@loader_path/Frameworks'");
+                var targetName = PBXProject.GetUnityTargetName();
+                var targetGuid = pbxProj.TargetGuidByName(targetName);
+                pbxProj.SetBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "$(inherited) '@executable_path/Frameworks' '@loader_path/Frameworks'");
 
-            pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+                pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+            }
         }
 
         [PostProcessBuild]
         public static void AddCopyFrameworkBuildPhase(BuildTarget buildTarget, string pathToBuiltProject)
         {
-            var pbxProj = new PBXProject();
-            pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
-
-            var targetName = PBXProject.GetUnityTargetName();
-            var targetGuid = pbxProj.TargetGuidByName(targetName);
-
-            var phaseGuid = pbxProj.AddCopyFilesBuildPhase(targetGuid, "Copy Pilgrim Framework", "", "10");
-            var frameworkGuid = pbxProj.FindFileGuidByProjectPath("Frameworks/PilgrimUnitySDK/Plugins/iOS/Pilgrim.framework");
-            pbxProj.AddFileToBuildSection(targetGuid, phaseGuid, frameworkGuid);
-
-            pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
-
-            var pbxProjContents = File.ReadAllText(PBXProject.GetPBXProjectPath(pathToBuiltProject));
-            var pattern = "([0-9A-Z]+) /\\* Pilgrim.framework in Copy Pilgrim Framework \\*/ = {isa = PBXBuildFile; fileRef = ([0-9A-Z]+) /\\* Pilgrim.framework \\*/; };";
-            var replacement = "$1 /* Pilgrim.framework in Copy Pilgrim Framework */ = {isa = PBXBuildFile; fileRef = $2 /* Pilgrim.framework */; settings = {ATTRIBUTES = (CodeSignOnCopy, ); }; };";
-            var regex = new Regex(pattern);
-            var match = regex.Match(pbxProjContents);
-            if (match.Success)
+            if (buildTarget == BuildTarget.iOS)
             {
-                pbxProjContents = regex.Replace(pbxProjContents, replacement);
-            }
-            else
-            {
-                Debug.LogError("Error enabling 'Code Sign on Copy' for 'Copy Pilgrim Framework' build phase, enable manually.");
-            }
+                var pbxProj = new PBXProject();
+                pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
 
-            File.WriteAllText(PBXProject.GetPBXProjectPath(pathToBuiltProject), pbxProjContents);
+                var targetName = PBXProject.GetUnityTargetName();
+                var targetGuid = pbxProj.TargetGuidByName(targetName);
+
+                var phaseGuid = pbxProj.AddCopyFilesBuildPhase(targetGuid, "Copy Pilgrim Framework", "", "10");
+                var frameworkGuid = pbxProj.FindFileGuidByProjectPath("Frameworks/PilgrimUnitySDK/Plugins/iOS/Pilgrim.framework");
+                pbxProj.AddFileToBuildSection(targetGuid, phaseGuid, frameworkGuid);
+
+                pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+
+                var pbxProjContents = File.ReadAllText(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+                var pattern = "([0-9A-Z]+) /\\* Pilgrim.framework in Copy Pilgrim Framework \\*/ = {isa = PBXBuildFile; fileRef = ([0-9A-Z]+) /\\* Pilgrim.framework \\*/; };";
+                var replacement = "$1 /* Pilgrim.framework in Copy Pilgrim Framework */ = {isa = PBXBuildFile; fileRef = $2 /* Pilgrim.framework */; settings = {ATTRIBUTES = (CodeSignOnCopy, ); }; };";
+                var regex = new Regex(pattern);
+                var match = regex.Match(pbxProjContents);
+                if (match.Success)
+                {
+                    pbxProjContents = regex.Replace(pbxProjContents, replacement);
+                }
+                else
+                {
+                    Debug.LogError("Error enabling 'Code Sign on Copy' for 'Copy Pilgrim Framework' build phase, enable manually.");
+                }
+
+                File.WriteAllText(PBXProject.GetPBXProjectPath(pathToBuiltProject), pbxProjContents);
+            }
+        }
+
+        [PostProcessBuild]
+        public static void AddStripFrameworksBuildPhase(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            if (buildTarget == BuildTarget.iOS)
+            {
+                var pbxProj = new PBXProject();
+                pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+
+                var targetName = PBXProject.GetUnityTargetName();
+                var targetGuid = pbxProj.TargetGuidByName(targetName);
+
+                pbxProj.AddShellScriptBuildPhase(targetGuid, "Pilgrim Strip Frameworks", "/bin/sh", "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/Pilgrim.framework/strip-frameworks.sh");
+
+                pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+            }
         }
 
         [PostProcessBuild]
         public static void EnsureCoreLocationIsAdded(BuildTarget buildTarget, string pathToBuiltProject)
         {
-            var pbxProj = new PBXProject();
-            pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
-
-            var targetName = PBXProject.GetUnityTargetName();
-            var targetGuid = pbxProj.TargetGuidByName(targetName);
-
-            if (!pbxProj.ContainsFramework(targetGuid, "CoreLocation.framework"))
+            if (buildTarget == BuildTarget.iOS)
             {
-                pbxProj.AddFrameworkToProject(targetGuid, "CoreLocation.framework", false);
-            }
+                var pbxProj = new PBXProject();
+                pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
 
-            pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+                var targetName = PBXProject.GetUnityTargetName();
+                var targetGuid = pbxProj.TargetGuidByName(targetName);
+
+                if (!pbxProj.ContainsFramework(targetGuid, "CoreLocation.framework"))
+                {
+                    pbxProj.AddFrameworkToProject(targetGuid, "CoreLocation.framework", false);
+                }
+
+                pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+            }
         }
 
     }
