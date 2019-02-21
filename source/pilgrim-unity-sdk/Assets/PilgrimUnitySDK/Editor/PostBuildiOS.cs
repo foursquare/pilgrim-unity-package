b645f49fb92fba929442433642ae389040131e45
Brian Rojas <marchinram@gmail.com>
2019-02-20 18:44:30 -0600

    Added strip frameworks build phase for Unity versions lower than 2018.3

--- source/pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Editor/PostBuildiOS.cs
+++ source/pilgrim-unity-sdk/Assets/PilgrimUnitySDK/Editor/PostBuildiOS.cs
@@ -13,7 +13,7 @@ namespace Foursquare
     public static class PostBuildiOS
     {
 
-        [PostProcessBuild]
+        [PostProcessBuild(0)]
         public static void AddAlwaysAndWhenInUseUsageDescription(BuildTarget buildTarget, string pathToBuiltProject)
         {
             if (buildTarget == BuildTarget.iOS && PilgrimConfigSettings.GetBool(PilgrimConfigSettings.CopyWhenInUseToAlwaysKey, true))
@@ -35,7 +35,7 @@ namespace Foursquare
             }
         }
 
-        [PostProcessBuild]
+        [PostProcessBuild(1)]
         public static void AddRunPathSearchPaths(BuildTarget buildTarget, string pathToBuiltProject)
         {
             if (buildTarget == BuildTarget.iOS)
@@ -51,7 +51,7 @@ namespace Foursquare
             }
         }
 
-        [PostProcessBuild]
+        [PostProcessBuild(2)]
         public static void AddCopyFrameworkBuildPhase(BuildTarget buildTarget, string pathToBuiltProject)
         {
             if (buildTarget == BuildTarget.iOS)
@@ -86,11 +86,12 @@ namespace Foursquare
             }
         }
 
-        [PostProcessBuild]
+        [PostProcessBuild(3)]
         public static void AddStripFrameworksBuildPhase(BuildTarget buildTarget, string pathToBuiltProject)
         {
             if (buildTarget == BuildTarget.iOS)
             {
+#if UNITY_2018_3_OR_NEWER
                 var pbxProj = new PBXProject();
                 pbxProj.ReadFromFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
 
@@ -98,12 +99,57 @@ namespace Foursquare
                 var targetGuid = pbxProj.TargetGuidByName(targetName);
 
                 pbxProj.AddShellScriptBuildPhase(targetGuid, "Pilgrim Strip Frameworks", "/bin/sh", "${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/Pilgrim.framework/strip-frameworks.sh");
-
                 pbxProj.WriteToFile(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+#else
+                var buffer = new byte[12];
+                new System.Random().NextBytes(buffer);
+                var phaseGuid = System.BitConverter.ToString(buffer).Replace("-", "");
+
+                var pbxProjContents = File.ReadAllText(PBXProject.GetPBXProjectPath(pathToBuiltProject));
+
+                var pattern = "/\\* Copy Pilgrim Framework \\*/,";
+                var replacement = string.Format("$0 {0} /* Pilgrim Strip Frameworks */,", phaseGuid);
+                var regex = new Regex(pattern);
+                var match = regex.Match(pbxProjContents);
+                if (match.Success)
+                {
+                    pbxProjContents = regex.Replace(pbxProjContents, replacement);
+                }
+                else
+                {
+                    Debug.LogError("Error adding strip frameworks run script build phase, add manually.");
+                }
+
+                pattern = "/\\* Begin PBXShellScriptBuildPhase section \\*/";
+                replacement = string.Format(@"$0
+                {0} /* Pilgrim Strip Frameworks */ = {{
+			        isa = PBXShellScriptBuildPhase;
+			        buildActionMask = 2147483647;
+			        files = (
+			        );
+			        name = ""Pilgrim Strip Frameworks"";
+			        runOnlyForDeploymentPostprocessing = 0;
+			        shellPath = /bin/sh;
+			        shellScript = ""${{BUILT_PRODUCTS_DIR}}/${{FRAMEWORKS_FOLDER_PATH}}/Pilgrim.framework/strip-frameworks.sh"";
+		        }};", phaseGuid);
+
+                regex = new Regex(pattern);
+                match = regex.Match(pbxProjContents);
+                if (match.Success)
+                {
+                    pbxProjContents = regex.Replace(pbxProjContents, replacement);
+                }
+                else
+                {
+                    Debug.LogError("Error adding strip frameworks run script build phase, add manually.");
+                }
+
+                File.WriteAllText(PBXProject.GetPBXProjectPath(pathToBuiltProject), pbxProjContents);
+#endif
             }
         }
 
-        [PostProcessBuild]
+        [PostProcessBuild(4)]
         public static void EnsureCoreLocationIsAdded(BuildTarget buildTarget, string pathToBuiltProject)
         {
             if (buildTarget == BuildTarget.iOS)
